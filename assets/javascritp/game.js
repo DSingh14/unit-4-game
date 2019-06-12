9c664f67976c486c64223595d25b642c7d01d72b
Deepali Singh <14singh.deepali@gmail.com>
2019-06-12 10:04:07 -0500

    initial commit

--- assets/javascritp/game.js
+++ assets/javascritp/game.js
@@ -1,19 +1,23 @@
 
 $(document).ready(function () {
-    // a Target random number
-    $("#random-button").on("click", function () {
+    // // a Target random number
+    // $("#random-button").on("click", function () {
 
-        var randomTarget = Math.floor(Math.random() * 120) + 19;
+    //     var randomTarget = Math.floor(Math.random() * 120) + 19;
 
-        // dump the random number into our random-number div.
-        $("#target-count").text(randomTarget);
-    });
+    //     // dump the random number into our random-number div.
+    //     $("#target-count").text(randomTarget);
+    // });
 
-    // a Crystal random number
+    // a Crystal and target random number. Display target number
     var crystal1 = Math.floor(Math.random() * 12 + 1);
     var crystal2 = Math.floor(Math.random() * 12 + 1);
     var crystal3 = Math.floor(Math.random() * 12 + 1);
     var crystal4 = Math.floor(Math.random() * 12 + 1);
+    var randomTarget = Math.floor(Math.random() * 120) + 19;
+    $("#target-count").text(randomTarget);
+
+
 
     // var win/loss/total score
     var wins = 0;
@@ -25,67 +29,67 @@ $(document).ready(function () {
 
     //reset conditions
     function reset() {
-        randomTarget = Math.floor(Math.random() * 120) + 19;
+        randomTarget = Math.floor(Math.random() * 102) + 19;
         $("#target-count").text(randomTarget);
-        var crystal1 = Math.floor(Math.random() * 12 + 1);
-        var crystal2 = Math.floor(Math.random() * 12 + 1);
-        var crystal3 = Math.floor(Math.random() * 12 + 1);
-        var crystal4 = Math.floor(Math.random() * 12 + 1);
-        $("#totalScore").text(sumOfScore);
+        crystal1 = Math.floor(Math.random() * 12 + 1);
+        crystal2 = Math.floor(Math.random() * 12 + 1);
+        crystal3 = Math.floor(Math.random() * 12 + 1);
+        crystal4 = Math.floor(Math.random() * 12 + 1);
         sumOfScore = 0;
+        $("#totalScore").text(sumOfScore);
     }
     // adding crystal value
     $("#yellowD").on("click", function () {
         sumOfScore = sumOfScore + crystal1;
         $("#totalScore").text(sumOfScore);
-        if (totalScore === randomTarget) {
+        if (sumOfScore === randomTarget) {
             wins++;
             $("#wins").text(wins);
             reset();
         }
-        else if (totalScore > randomTarget) {
+        else if (sumOfScore > randomTarget) {
             losses++;
             $("#losses").text(losses);
             reset();
         }
     });
     $("#blueD").on("click", function () {
-        sumOfScore = sumOfScore + crystal1;
+        sumOfScore = sumOfScore + crystal2;
         $("#totalScore").text(sumOfScore);
-        if (totalScore === randomTarget) {
+        if (sumOfScore === randomTarget) {
             wins++;
             $("#wins").text(wins);
             reset();
         }
-        else if (totalScore > randomTarget) {
+        else if (sumOfScore > randomTarget) {
             losses++;
             $("#losses").text(losses);
             reset();
         }
     });
     $("#diamond").on("click", function () {
-        sumOfScore = sumOfScore + crystal1;
+        sumOfScore = sumOfScore + crystal3;
         $("#totalScore").text(sumOfScore);
-        if (totalScore === randomTarget) {
+        if (sumOfScore === randomTarget) {
             wins++;
             $("#wins").text(wins);
             reset();
         }
-        else if (totalScore > randomTarget) {
+        else if (sumOfScore > randomTarget) {
             losses++;
             $("#losses").text(losses);
             reset();
         }
     });
     $("#redD").on("click", function () {
-        sumOfScore = sumOfScore + crystal1;
+        sumOfScore = sumOfScore + crystal4;
         $("#totalScore").text(sumOfScore);
-        if (totalScore === randomTarget) {
+        if (sumOfScore === randomTarget) {
             wins++;
             $("#wins").text(wins);
             reset();
         }
-        else if (totalScore > randomTarget) {
+        else if (sumOfScore > randomTarget) {
             losses++;
             $("#losses").text(losses);
             reset();
